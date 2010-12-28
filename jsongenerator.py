415326437106614be8dfd8cce2d7e442dc35601d
Yuvi Panda <yuvipanda@gmail.com>
2010-12-29 03:28:06 +0530

    Added code to statically generate data json

---
+++ jsongenerator.py
@@ -0,0 +1,12 @@
+import sys
+import json
+
+from database import ComicsDB
+
+if __name__ == "__main__":
+    db = ComicsDB(sys.argv[1])
+    comics = db.get_all()
+    titles = ("id", "title", "alt", "image_src")
+    data = {"count": len(comics),
+            "comics": dict([(int(comic[0]), dict(zip(titles, comic))) for comic in comics])}
+    print json.dumps(data)
