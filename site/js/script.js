090c697f5c353dbd337ced4bddc8d4b9c578767c
Yuvi Panda <yuvipanda@gmail.com>
2010-12-31 03:21:11 +0530

    Moved site to use excerpts. Primitive Game Mechanics

--- site/js/script.js
+++ site/js/script.js
@@ -1,4 +1,5 @@
 var xkcd = null;
+var correctTitleChoice;
 
 var showImage = function(url) {
     var img = new Image();
@@ -17,25 +18,40 @@ var getComic = function() {
         var val = Math.floor(Math.random() * xkcd.count);
         $("#title-choice-" + (i+1)).html(xkcd.comics[val].title);
     }
-    var correctTitleChoice = Math.floor(1 + (Math.random() * 5));
+    correctTitleChoice = Math.floor(1 + (Math.random() * 5));
     $("#title-choice-" + correctTitleChoice).html(xkcd.comics[selID].title);
-    
-    for(var i=0; i < 5; i++) {
-        var val = Math.floor(Math.random() * xkcd.count);
-        $("#alt-choice-" + (i+1)).html(xkcd.comics[val].alt);
-    }
-    var correctAltChoice = Math.floor(1 + (Math.random() * 5));
-    $("#alt-choice-" + correctAltChoice).html(xkcd.comics[selID].alt);
-    $("#game-area").fadeOut("fast");
-    showImage(xkcd.comics[selID].image_src);
+    $("#exerpt").html(extractExerpt(xkcd.comics[selID].transcript));
+    $("#status").fadeOut("fast");
 }
 
+var extractExerpt = function(transcript) {
+    var lines = transcript.split("\n");
+    var selID = Math.floor(Math.random() * lines.length);
+    var selectedLine = lines[selID];
+    if (selectedLine.length > 15) {
+        return lines[selID];
+    }
+    else {
+        return extractExerpt(transcript);
+    }
+}
+
+var checkAnswer = function() {
+    if ($(this).attr("id") == "title-choice-" + correctTitleChoice) {
+        $("#status").html("Correct Answer!");
+    }
+    else {
+        $("#status").html("Wrong Answer!");
+    }
+    $("#status").fadeIn("fast");
+}
 var init = function() {
     $.getJSON("xkcd.json", 
             function(data) { 
             xkcd = data;
             getComic(data);
             $("#reload").click(getComic);
+            $("#title-choices span").click(checkAnswer);
             });
  }
 
