b0ab53cb05727eaf8b6627ea41781c24336a44a2
Yuvi Panda <yuvipanda@gmail.com>
2010-12-31 02:20:31 +0530

    JSON Generator now outputs transcript

--- jsongenerator.py
+++ jsongenerator.py
@@ -6,7 +6,7 @@ from database import ComicsDB
 if __name__ == "__main__":
     db = ComicsDB(sys.argv[1])
     comics = db.get_all()
-    titles = ("id", "title", "alt", "image_src")
+    titles = ("id", "title", "alt", "transcript", "image_src")
     data = {"count": len(comics),
             "comics": dict([(int(comic[0]), dict(zip(titles, comic))) for comic in comics])}
     print json.dumps(data)
